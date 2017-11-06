4985255935783ab4b81d0e7b3913717774a5d428
jarvast <tamas.jarvas13@gmail.com>
2017-11-06 20:38:13 +0100

    kis database fix

--- etelrendeles/src/main/resources/data.sql
+++ etelrendeles/src/main/resources/data.sql
@@ -15,81 +15,81 @@ INSERT INTO CATEGORIES (NAME) VALUES ('Soup');
 INSERT INTO CATEGORIES (NAME) VALUES ('Salad');       
 INSERT INTO CATEGORIES (NAME) VALUES ('Drink');       
 
-//pizzak   
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Al Capone','ham, egg, clam, fish, cheese', 'false', 'false','1650' ,1); //1
+--pizzak
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Al Capone','ham, egg, clam, fish, cheese', 'false', 'false','1650' ,1);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Bali','tomato sauce, cheese, pineapple,chicken breast, corn', 'true', 'false', '1550' ,1);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Bolognese', 'tomato sauce, cheese, bolognai ragu, smoked cheese ', 'true', 'false', '1650' ,1);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Cesare', 'cheese, bacon, salami, egg, tomato', 'true', 'false', '1550' ,1);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Feta', 'cheese, sheep cheese, sour cream, garlic ', 'true', 'true', '1550' ,1);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Hawaii', 'tomato sauce, cheese, ham, pineapple', 'false', 'false', '1250' ,1);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('HamCorn,' 'cheese, ham, corn', 'false', 'false', '1450' ,1);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('HamCorn', 'cheese, ham, corn', 'false', 'false', '1450' ,1);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Maffioso', 'ham, mushroom, sausage, pfefferoni, cheese ', 'true', 'false', '1650' ,1);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Magyaros', 'cheese, sausage, paprika, tomato, onion', 'true', 'false', '1450' ,1);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Mexicana', 'bolognai ragu, corn, chili beans, sour cream, pfefferoni, cheese', 'true', 'false', '1550' ,1);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('SonGoKu', 'ham, mushroom, corn, cheese', 'false', 'false', '1550' ,1); //11
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('SonGoKu', 'ham, mushroom, corn, cheese', 'false', 'false', '1550' ,1);
 
-//hamburgerek
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Buffalo burger', 'puffs, beef, vegetables, fried onions, smoked cheese, sauce', 'false', 'false', '1150' , 2); //12
+--hamburgerek
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Buffalo burger', 'puffs, beef, vegetables, fried onions, smoked cheese, sauce', 'false', 'false', '1150' , 2);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Chicken burger', 'puffs, roston chicken breast, vegetables, bacon, chicken liver, corn, smoked cheese, sauce, chicken breast fillet', 'false', 'false', '950' , 2);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Hawaii burger', 'puffs, roston chicken breast, vegetables, pineapple, cheese, mayonnaise, barbecue sauce, chicken breast fillet', 'false', 'false', '950' , 2);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Kentucky beef burger', 'puffs, beef, vegetables, bacon, grillezett cheese, garlic mayonnaise', 'false', 'false', '1250', 2);
 INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Hell burger', 'puffs, beef, vegetables, fried onion, jalapeno, cheddar sajt, smoked cheese, pepper sauce', 'false', 'false', '1350' , 2);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Röszti burger', 'puffs, beef, vegetables, dupla bacon, röszti, cheese, mustard, barbecue sauce', 'false', 'false', '850' , 2);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Vega burger', 'puffs, fried cheese, fried onions, salad, cabbage, cucumber, tomato, tartar sauce', 'false', 'false', '850' , 2); //18
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Roszti burger', 'puffs, beef, vegetables, dupla bacon, röszti, cheese, mustard, barbecue sauce', 'false', 'false', '850' , 2);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE, CATEGORY_ID) VALUES ('Vega burger', 'puffs, fried cheese, fried onions, salad, cabbage, cucumber, tomato, tartar sauce', 'false', 'false', '850' , 2);
 
-//levesek
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Bean soup', 'bowl', 'false', 'false', ,'850' , 3); //19
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('French onion soup', 'bowl', 'false', 'false', ,'750' , 3);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Goulash soup', 'bowl', 'false', 'false', ,'850' , 3);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Liver dumpling soup', 'bowl', 'false', 'false', ,'850' , 3);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Pickled turkey soup', 'bowl', 'false', 'false', ,'850' , 3); //23
+--levesek
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Bean soup', 'bowl', 'false', 'false', '850' , 3);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('French onion soup', 'bowl', 'false', 'false', '750' , 3);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Goulash soup', 'bowl', 'false', 'false', '850' , 3);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Liver dumpling soup', 'bowl', 'false', 'false', '850' , 3);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Pickled turkey soup', 'bowl', 'false', 'false', '850' , 3);
 
 
-//salaták
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Caesar', 'grilled chicken breast, ice lettuce, tomato, pepper, cucumber, crouton, ceasar dressing', 'false', 'false', ,'2000' , 4); //24
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Exotic salad', 'grilled chicken breast, ice lettuce, pineapple, banana, orange, toasted almonds', 'false', 'false', ,'1600' , 4);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Greek salad', 'sheep cheese, ice lettuce, tomatoes, peppers, cucumbers, red onions, olives, olive oil, lemon juice', 'false', 'true', ,'1500' , 4);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Mayonnaise corn salad', 'corn, curd, apple, tartar', 'false', 'true', ,'2000' , 4);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Grilled chicken salad', 'grilled chicken breast, tomato, pepper, cucumber, corn, olive oil, lemon juice', 'false', 'false', ,'1600' , 4);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Winged salad', 'cooked chicken breast, sheep cheese, egg, tomato, pepper, cucumber, onion dressing', 'false', 'false', ,'1600' , 4);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Tuna salad', 'tuna, tomatoes, peppers, cucumbers, olive oil, lemon juice, garlic and yoghurt sauce', 'false', 'false', ,'1500' , 4);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Tzatziki salad', 'kefir, sour cream, cottage cheese, cucumber, garlic, crouton', 'false', 'true', ,'1400' , 4); //31
+--salaták
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Caesar', 'grilled chicken breast, ice lettuce, tomato, pepper, cucumber, crouton, ceasar dressing', 'false', 'false', '2000' , 4);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Exotic salad', 'grilled chicken breast, ice lettuce, pineapple, banana, orange, toasted almonds', 'false', 'false', '1600' , 4);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Greek salad', 'sheep cheese, ice lettuce, tomatoes, peppers, cucumbers, red onions, olives, olive oil, lemon juice', 'false', 'true', '1500' , 4);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Mayonnaise corn salad', 'corn, curd, apple, tartar', 'false', 'true', '2000' , 4);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Grilled chicken salad', 'grilled chicken breast, tomato, pepper, cucumber, corn, olive oil, lemon juice', 'false', 'false', '1600' , 4);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Winged salad', 'cooked chicken breast, sheep cheese, egg, tomato, pepper, cucumber, onion dressing', 'false', 'false', '1600' , 4);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Tuna salad', 'tuna, tomatoes, peppers, cucumbers, olive oil, lemon juice, garlic and yoghurt sauce', 'false', 'false', '1500' , 4);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Tzatziki salad', 'kefir, sour cream, cottage cheese, cucumber, garlic, crouton', 'false', 'true','1400' , 4);
 
-//italok
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Water', ' 0,5l', 'false', 'false', ,'50' , 5); //32
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Borsodi',' 0,5l', 'false', 'false', ,'300' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Arany Aszok', ' 0,5l', 'false', 'false', ,'350' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Heineken', ' 0,5l', 'false', 'false', ,'450' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Soproni', ' 0,5l', 'false', 'false', ,'350' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Dreher',' 0,5l', 'false', 'false', ,'400' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Unicum',' 0,3cl', 'false', 'false', ,'400' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Jagermeister',' 0,3l', 'false', 'false', ,'5' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Vodka',' 0,5l', 'false', 'false', ,'400' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Brandy',' 0,5l', 'false', 'false', ,'400' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Red wine',' 0,3l', 'false', 'false', ,'250' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('White wine',' 0,3l', 'false', 'false', ,'250' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Rose',' 0,3l', 'false', 'false', ,'250' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Pepsi',' 0,33l', 'false', 'false', ,'400' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Coca Cola',' 0,33l', 'false', 'false', ,'400' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Sprite',' 0,33l', 'false', 'false', ,'400' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Fanta',' 0,33l', 'false', 'false', ,'400' , 5);
-INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Kenley',' 0,33l', 'false', 'false', ,'400' , 5); //49
+--italok
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Water', ' 0,5l', 'false', 'false', '50' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Borsodi',' 0,5l', 'false', 'false', '300' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Arany Aszok', ' 0,5l', 'false', 'false', '350' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Heineken', ' 0,5l', 'false', 'false', '450' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Soproni', ' 0,5l', 'false', 'false', '350' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Dreher',' 0,5l', 'false', 'false', '400' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Unicum',' 0,3cl', 'false', 'false', '400' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Jagermeister',' 0,3l', 'false', 'false', '5' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Vodka',' 0,5l', 'false', 'false', '400' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Brandy',' 0,5l', 'false', 'false', '400' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Red wine',' 0,3l', 'false', 'false', '250' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('White wine',' 0,3l', 'false', 'false', '250' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Rose',' 0,3l', 'false', 'false', '250' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Pepsi',' 0,33l', 'false', 'false', '400' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Coca Cola',' 0,33l', 'false', 'false', '400' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Sprite',' 0,33l', 'false', 'false', '400' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Fanta',' 0,33l', 'false', 'false', '400' , 5);
+INSERT INTO ITEMS (NAME, DESCRIPTION, IS_SPICY, IS_VEGETARIAN, PRICE,CATEGORY_ID) VALUES ('Kenley',' 0,33l', 'false', 'false', '400' , 5);
 
 
 INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,STATUS,USER_ID) VALUES ('Hudini', 'Water str 12.', '06206666666',current_timestamp,'IDLE',1);
 INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,DELIVERED,STATUS,USER_ID) VALUES ('Jarvas', 'Fire str 21.', '06305555555',current_timestamp,current_timestamp,'DELIVERED',2);
 INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,STATUS,USER_ID) VALUES ('MizikK', 'Wind str 5.', '06703333333',current_timestamp,'IDLE',3);
-INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,STATUS,USER_ID) VALUES ('Hudini', 'Water str 12.', '06206666666',current_timestamp,'IDLE',4);
+INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,STATUS,USER_ID) VALUES ('Hudini', 'Water str 12.', '06206666666',current_timestamp,'IDLE',1);
 INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,STATUS,USER_ID) VALUES ('GyCsaba', 'Cat str 37.', '062039778315',current_timestamp,'IDLE',5);
 INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,STATUS,USER_ID) VALUES ('NagyB', 'Nun str 7.', '06203737373',current_timestamp,'IDLE',6);
 INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,STATUS,USER_ID) VALUES ('NagyBrigi', 'Cat str 13.', '06704566543',current_timestamp,'IDLE',7);
-INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,DELIVERED,STATUS,USER_ID) VALUES ('Jarvas', 'Fire str 21.', '06305555555',current_timestamp,current_timestamp,'DELIVERED',8);
+INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,DELIVERED,STATUS,USER_ID) VALUES ('Jarvas', 'Fire str 21.', '06305555555',current_timestamp,current_timestamp,'DELIVERED',2);
 INSERT INTO ORDERS (NAME, ADDRESS, PHONE, RECEIVED,STATUS,USER_ID) VALUES ('KovacsP', 'Bucka str 42.', '06708884888',current_timestamp,'IDLE',9);
 
 
 
 INSERT INTO ORDER_ITEMS (ORDER_ID, ITEM_ID) VALUES (1,10);
-INSERT INTO ORDER_ITEMS (ORDER_ID, ITEM_ID) VALUES (1,49);
+INSERT INTO ORDER_ITEMS (ORDER_ID, ITEM_ID) VALUES (1,48);
 INSERT INTO ORDER_ITEMS (ORDER_ID, ITEM_ID) VALUES (2,2);
 INSERT INTO ORDER_ITEMS (ORDER_ID, ITEM_ID) VALUES (3,12);
 INSERT INTO ORDER_ITEMS (ORDER_ID, ITEM_ID) VALUES (3,45);
